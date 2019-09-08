cc04033ed83bbca38c49fbcbdb290571bb9b23b6
ngoclq <ngoclq@tech.est-rouge.com>
2019-09-08 23:27:07 +0700

    install mongoose & connect mongoDB

--- src/api/src/index.js
+++ src/api/src/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import configs from './constants/configs-dev';
 
 const app = express();
+
+/** PORT NUMBER */
+const PORT = process.env.Portal || 5000;
+
+/**
+ * MongoDB Connect
+ */
+mongoose.connect(configs.MONGO_URI, { useNewUrlParser: true})
+    .then( () => console.log('MongoDB Connected'))
+    .catch( err => console.log(err));
 
 app.post('/api/auth', (req, res) => {
     res.status(400).json({
@@ -13,4 +25,4 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(8080, () => console.log('API SERVER Running on localhost:8080'));
+app.listen(8080, () => console.log(`API SERVER Running on localhost:${PORT}`));
